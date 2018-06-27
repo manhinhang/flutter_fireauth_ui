232df21a597767f6075ece567f47264c270b3b67
francesc <francesc3000@gmail.com>
2018-06-27 22:41:15 +0200

    Spanish and Catala translations

---
+++ lib/l10n/messages_es.dart
@@ -0,0 +1,39 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+// ignore: unused_element
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+// ignore: non_constant_identifier_names
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => 'es';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "continueText" : MessageLookupByLibrary.simpleMessage("Continuar"),
+    "displayName" : MessageLookupByLibrary.simpleMessage("Nombre de usuario"),
+    "displayNameHint" : MessageLookupByLibrary.simpleMessage("Introduzca su nombre de usuario"),
+    "email" : MessageLookupByLibrary.simpleMessage("Correo"),
+    "emailHint" : MessageLookupByLibrary.simpleMessage("Introduce tu correo"),
+    "emailSignInButton" : MessageLookupByLibrary.simpleMessage("Registrarse con correo"),
+    "emptyEmailWarning" : MessageLookupByLibrary.simpleMessage("Por favor entra tu correo"),
+    "error" : MessageLookupByLibrary.simpleMessage("Error"),
+    "facebookSignInButton" : MessageLookupByLibrary.simpleMessage("Registrarse con Facebook"),
+    "forgotPassword" : MessageLookupByLibrary.simpleMessage("Olvidaste la contraseña ? "),
+    "googleSignInButton" : MessageLookupByLibrary.simpleMessage("Registrarse con Google"),
+    "invalidEmailWarning" : MessageLookupByLibrary.simpleMessage("Correo no válido"),
+    "okay" : MessageLookupByLibrary.simpleMessage("De acuerdo"),
+    "password" : MessageLookupByLibrary.simpleMessage("Contraseña *"),
+    "signIn" : MessageLookupByLibrary.simpleMessage("Registrarse"),
+    "signUp" : MessageLookupByLibrary.simpleMessage("Regístrate")
+  };
+}
